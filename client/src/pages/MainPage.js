9b3053f00a4995cef87c1cd90ce08b062c8f1fb8
Nuran <ranawakanuran@gmail.com>
2024-04-10 13:22:42 +0530

    Add client folder again

---
+++ client/src/pages/MainPage.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function MainPage() {
+  return (
+    <div>
+      <h1 className="lg:mx-32 text-5xl font-bold text-green-500">
+        Currency Exchange Calculator
+      </h1>
+      <p className="lg:mx-32 py-6 opacity-40">
+        Welcome to the future of seamless currency exchange – Whether you're a
+        savvy traveler, a global business professional, or simply someone
+        managing international transactions, our app is designed to simplify and
+        optimize your currency exchange experience like never before.
+      </p>
+    </div>
+  );
+}
